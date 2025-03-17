fe85a11ac91205022cabe370e5859cf4135ea859
shilong.song <shilong.song@wxbester.cn>
2025-03-17 16:36:32 +0800

    1

--- Leetcode/118.cpp
+++ Leetcode/118.cpp
@@ -1,9 +1,19 @@
 // C++二维数组
+// 杨辉三角
 #include <vector>
 class Solution
 {
 public:
     std::vector<std::vector<int>> generate(int numRows)
     {
+        std::vector<std::vector<int>> vec;
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                // 构建杨辉三角每一行数组
+                std::vector<int>
+            }
+        }
     }
 };
--- Leetcode/35.cpp
+++ Leetcode/35.cpp
@@ -9,16 +9,43 @@ class Solution
 public:
     int searchInsert(std::vector<int> &nums, int target)
     {
-        auto hi = nums.size();
-        decltype(nums.size()) low = 0;
-        auto mid = hi / 2;
-        // low == hi时，指向最后一个元素
-        while ()
-        {
-        }
+        auto begin = nums.begin();
+        auto end = nums.end() - 1;
+        auto mid = begin + (end - begin) / 2;
 
-        // 没找到 按顺序插入
+        while (lo < hi)
+        {
+            int mi = (lo + hi) >> 1;
+            if(e<A[mi]) hi = mi;
+            else if (A[mi]) lo =mi + 1;
+            else  return mi;
+            /* code */
+        }
+        
+        // low == hi时，指向最后一个元素
     }
 };
 
 // 本体主要考虑查找，O(logn)时间复杂度的查找 二分查找
+/*
+        auto hi = nums.size() - 1;
+decltype(nums.size()) low = 0;
+while (low <= hi)
+{
+    auto mid = low + (hi - low) / 2;
+    if (nums[mid] == target)
+    {
+        return mid;
+    }
+    else if (nums[mid] < target)
+    {
+        low = mid + 1;
+    }
+    else
+    {
+        hi = mid - 1;
+    }
+}
+// 没找到 按顺序插入
+return low;
+*/
