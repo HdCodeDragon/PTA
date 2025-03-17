55c414b03cd56692dc78cbf1ab7893a98bc12e0e
shilong.song <shilong.song@wxbester.cn>
2025-03-17 08:38:14 +0800

    1

---
+++ Leetcode/217.cpp
@@ -0,0 +1,11 @@
+// 最复杂，O(n*n) ((n-1) + 1) * (n-1) / 2
+// 去重，无序去重，有序去重
+// 排序之后双指针 O(nlogn)
+#include <vector>
+class Solution
+{
+public:
+    bool containsDuplicate(std::vector<int> &nums)
+    {
+    }
+};
