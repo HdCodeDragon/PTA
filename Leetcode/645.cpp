2ef89ef1bf61f4179cbc67c13125b6d6a1bf607c
shilong.song <shilong.song@wxbester.cn>
2025-03-21 16:16:47 +0800

    1

---
+++ Leetcode/645.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+// 如何找出重复的数字和丢失的数字
+// 找重复，二分查找O(nlogn) 在找重复的情况下找缺失 1-n
+// 每遍历一次判断一次是否增1，不然就是缺少
+class Solution
+{
+public:
+    std::vector<int> findErrorNums(std::vector<int> &nums)
+    {
+    }
+};
