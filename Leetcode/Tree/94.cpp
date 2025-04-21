564fbdb72094758ef96d23ea204fe75737ab438e
shilong.song <shilong.song@wxbester.cn>
2025-04-21 17:07:48 +0800

    1

--- Leetcode/Tree/94.cpp
+++ Leetcode/Tree/94.cpp
@@ -33,3 +33,15 @@ public:
         // ÓÒ
     }
 };
+
+// ÖÐÐò±éÀúµü´ú
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
